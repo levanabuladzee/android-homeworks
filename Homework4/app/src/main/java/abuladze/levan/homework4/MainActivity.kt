04eb7d84da9f0783f0437f58e271404a35175af5
Levan Abuladze <46747473+levanabuladzee@users.noreply.github.com>
2020-11-13 22:44:47 +0400

    added homework 4

---
+++ Homework4/app/src/main/java/abuladze/levan/homework4/MainActivity.kt
@@ -0,0 +1,38 @@
+package abuladze.levan.homework4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter()
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        val pager = findViewById<ViewPager2>(R.id.pager)
+        pager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, pager) { tab: TabLayout.Tab, position: Int ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
